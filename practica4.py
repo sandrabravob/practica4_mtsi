bf559b5803ad0bff7a80dec45f264148a97ecc8b
am_uah <adolfo.moratalla@edu.uah.es>
2019-04-19 19:04:15 +0200

    prueba

--- practica4.py
+++ practica4.py
@@ -3,7 +3,9 @@ import csv, operator
 # Leer el archivo 'datos.csv' con reader() y 
 # mostrar todos los registros, uno a uno:
 
+
 with open('ADBE.csv') as csvarchivo:
     entrada = csv.reader(csvarchivo)
     for reg in entrada:
         print(reg)  # Cada línea se muestra como una lista de campos
+
